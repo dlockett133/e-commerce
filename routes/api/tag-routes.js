b0c128f5eef39a85c65f360941885f334e2a4aca
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-02-21 21:07:37 -0500

    created a put route for updating a tag's name by its id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -47,12 +47,25 @@ router.post('/', async (req, res) => {
     res.status(201).json(newTag);
   } catch (error) {
     console.log(error);
-    res.status(500).json({message: error.message})
+    res.status(500).json({message: error.message});
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const {id} = req.params;
+  const {tag_name} = req.body
+  try {
+    const updateTag = await Tag.update({tag_name}, {where: {id}});
+    if(updateTag[0] === 0){
+      console.log(`Can't find tag with an id of ${id}`);
+      res.status(404).json({message: `Can't find tag with an id of ${id}`})
+    }
+    res.status(200).json(updateTag);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: error.message});
+  }
 });
 
 router.delete('/:id', (req, res) => {
